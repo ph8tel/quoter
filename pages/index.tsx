be1d4eb90054540b73f290be74d2367b2b3c117e
Joe Jackson <ph8tel@gmail.com>
2022-10-30 10:45:59 -0600

    code cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-// @ts-nocheck
 import { useState } from 'react';
 import { Select, NumberInput, Stepper, Button, Group, TextInput, Code } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import axios from 'axios'
-export default function Demo() {
+export default function AiQuiz() {
   const [active, setActive] = useState(0);
 
   const [prediction, setPrediction] = useState('Press the button to ask Covertron');
@@ -15,15 +14,14 @@ export default function Demo() {
       url: 'https://aiquoter.herokuapp.com/ai',
       params: form.values
     });
-    console.log('yo0', answer.data.choices)
     setPrediction(answer.data.choices)
-    }
+  }
   const form = useForm({
     initialValues: {
       lob: 'Auto',
       drivingrecord: '',
       zipcode: '90210',
-      age: '35',
+      age: '',
       sex: '',
       assetValue: '',
       coverageLevel: '',
@@ -41,11 +39,10 @@ export default function Demo() {
 
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
-
   return (
     <>
-        <Stepper active={active} breakpoint="sm">
-          <Stepper.Step label="What and Where" description="What you want to insure and where">
+      <Stepper active={active} breakpoint="sm">
+        <Stepper.Step label="What and Where" description="What you want to insure and where">
           <Select
             label="How is your driving record?"
             placeholder="Pick one"
@@ -61,61 +58,61 @@ export default function Demo() {
         </Stepper.Step>
 
         <Stepper.Step label="About you" description="Tell us about you">
-        <NumberInput
-          defaultValue={18}
-          placeholder="Your age"
-          label="How old are you?"
-          withAsterisk
-          {...form.getInputProps('age')}
-        />
-        <Select
-          label="Male or Female"
-          placeholder="Male or Female?"
-          data={[
-            { value: 'male', label: 'Male' },
-            { value: 'female', label: 'Female' },
-          ]}
-          {...form.getInputProps('sex')}
-        />
+          <NumberInput
+            defaultValue={35}
+            placeholder="Your age"
+            label="How old are you?"
+            withAsterisk
+            {...form.getInputProps('age')}
+          />
+          <Select
+            label="Male or Female"
+            placeholder="Male or Female?"
+            data={[
+              { value: 'male', label: 'Male' },
+              { value: 'female', label: 'Female' },
+            ]}
+            {...form.getInputProps('sex')}
+          />
           <Select
             label="What is your credit score?"
             placeholder="Be honest"
             data={[
               { value: 'excellent', label: '800+' },
               { value: 'great', label: '750+' },
-              { value: 'ok', label: '700+'},
-              { value: 'bad', label: '650+'},
-              { value: 'horrid', label: 'Working on it, show me the high rates'}
+              { value: 'ok', label: '700+' },
+              { value: 'bad', label: '650+' },
+              { value: 'horrid', label: 'Working on it, show me the high rates' }
             ]}
             {...form.getInputProps('rating')}
           />
         </Stepper.Step>
+
         <Stepper.Step label="Coverage level" description="How covered do you want to be">
-        <Select
+          <Select
             label="How expensive is your car?"
             placeholder="Be honest"
             data={[
               { value: '80,000', label: 'Luxury' },
               { value: '50,000', label: 'Highly' },
-              { value: '35,000', label: 'Average'},
-              { value: '25,000', label: 'Economy'},
-              { value: '15,000', label: 'Compact'}
+              { value: '35,000', label: 'Average' },
+              { value: '25,000', label: 'Economy' },
+              { value: '15,000', label: 'Compact' }
             ]}
             {...form.getInputProps('assetValue')}
           />
-        <Select
+          <Select
             label="How much coverage?"
             placeholder="Cheap or the works?"
             data={[
               { value: 'full', label: 'The Works' },
-              { lable: 'decent', value: 'Decent' },
-              { value: 'minimal', label: 'The cheapest I can get'},
-              { value: '25,000', label: 'economy'},
-              { value: '15,000', label: 'compact'}
+              { lable: 'average', value: 'Decent' },
+              { value: 'minimal', label: 'The cheapest I can get' },
             ]}
             {...form.getInputProps("coverageLevel")}
           />
         </Stepper.Step>
+
         <Stepper.Completed>
           <Button onClick={askAi}>ask Covertron</Button>
           {prediction}
@@ -133,7 +130,6 @@ export default function Demo() {
         )}
         {active !== 3 && <Button onClick={nextStep}>Next step</Button>}
       </Group>
-    
     </>
   );
 }
